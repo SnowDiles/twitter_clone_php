c9a049580c69cc3d811cf045c4de03641973712e
Augustin Verissimo <augustin.verissimo@epitech.eu>
2025-02-17 14:30:35 +0100

    update database

--- db.sql
+++ db.sql
@@ -1,22 +1,29 @@
-CREATE TABLE User (
+CREATE TABLE Themes (
+    theme_id INT PRIMARY KEY AUTO_INCREMENT,
+    theme_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Media (
+    media_id INT PRIMARY KEY AUTO_INCREMENT,
+    media_url VARCHAR(255) UNIQUE NOT NULL,
+    short_url VARCHAR(8) UNIQUE NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash CHAR(40) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     bio TEXT,
     profile_picture_id INT,
-    theme_id INT DEFAULT 1, -- Si rien n'est donné à la création, prendre le premier thème de la BDD
+    theme_id INT DEFAULT 1,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (profile_picture_id) REFERENCES Media(media_id),
     FOREIGN KEY (theme_id) REFERENCES Themes(theme_id)
 );
 
-CREATE TABLE Theme (
-    theme_id INT PRIMARY KEY AUTO_INCREMENT,
-    theme_name VARCHAR(50) UNIQUE NOT NULL
-);
-
-CREATE TABLE Post (
+CREATE TABLE Posts (
     post_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     content VARCHAR(140) NOT NULL,
@@ -28,7 +35,7 @@ CREATE TABLE Post (
     FOREIGN KEY (repost_of) REFERENCES Posts(post_id)
 );
 
-CREATE TABLE Hashtag (
+CREATE TABLE Hashtags (
     hashtag_id INT PRIMARY KEY AUTO_INCREMENT,
     tag VARCHAR(100) UNIQUE NOT NULL
 );
@@ -41,7 +48,7 @@ CREATE TABLE PostHashtag (
     FOREIGN KEY (hashtag_id) REFERENCES Hashtags(hashtag_id)
 );
 
-CREATE TABLE Mention (
+CREATE TABLE Mentions (
     mention_id INT PRIMARY KEY AUTO_INCREMENT,
     post_id INT NOT NULL,
     user_id INT NOT NULL,
@@ -49,8 +56,8 @@ CREATE TABLE Mention (
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Follow (
-    follower_id INT NOT NULl,
+CREATE TABLE Follows (
+    follower_id INT NOT NULL,
     following_id INT NOT NULL,
     followed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (follower_id, following_id),
@@ -58,7 +65,7 @@ CREATE TABLE Follow (
     FOREIGN KEY (following_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Message (
+CREATE TABLE Messages (
     message_id INT PRIMARY KEY AUTO_INCREMENT,
     sender_id INT NOT NULL,
     receiver_id INT NOT NULL,
@@ -68,23 +75,16 @@ CREATE TABLE Message (
     FOREIGN KEY (receiver_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Media (
-    media_id INT PRIMARY KEY AUTO_INCREMENT,
-    media_url VARCHAR(255) UNIQUE NOT NULL,
-    short_url VARCHAR(8) UNIQUE NOT NULL,
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-);
-
-CREATE TABLE PostMedia {
+CREATE TABLE PostMedia (
     post_id INT NOT NULL,
     media_id INT NOT NULL,
-    FOREIGN KEY (post_id) REFERENCES Post(post_id),
-    FOREIGN KEY (media_id) REFERENCES Media(media_id),
-};
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id),
+    FOREIGN KEY (media_id) REFERENCES Media(media_id)
+);
 
-CREATE TABLE MessageMedia {
+CREATE TABLE MessageMedia (
     message_id INT NOT NULL,
     media_id INT NOT NULL,
-    FOREIGN KEY (message_id) REFERENCES Post(message_id),
-    FOREIGN KEY (media_id) REFERENCES Media(media_id),
-};
+    FOREIGN KEY (message_id) REFERENCES Messages(message_id),
+    FOREIGN KEY (media_id) REFERENCES Media(media_id)
+);
