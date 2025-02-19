bb02ac738187db37046bf85bdf590b033e62879a
Boulahia brahim <98060234+SnowDiles@users.noreply.github.com>
2025-02-19 16:18:13 +0100

    Feat/add of the main page front desktop and mobile (#16)
    * ADD/add the front of the login page
    
    * [fix] fix the problem of vertically center scroll on tablet
    
    * [fix]rename of the name branch
    
    ---------
    
    Co-authored-by: SnowDiles <brahimboulahiapro@gmail.com>
    Co-authored-by: Hokome <m.k.esseghir@protonmail.com>

---
+++ src/Views/auth/login.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../../dist/styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../../../public/css/login.css">
+
+</head>
+
+<body data-theme="my-custom-theme">
+    <div class="flex flex-col gap-10 justify-center items-center h-screen column" id="main">
+        <div>
+            <img src="../../../assets/icons/logo.png" alt="Logo" class="w-14 logo" />
+        </div>
+        <div>
+            <div>
+                <p>Inscrivez vous.</p>
+                <button class="btn variant-filled-primary text-secondary-500 rounded-lg">Créer un compte</button>
+            </div>
+            <div class="divider"></div>
+            <div>
+                <p>Vous avez deja un compte ?</p>
+                <button class="btn variant-outlined btn2">Se connecter</button>
+            </div>
+        </div>
+    </div>
+    <script src="../../../public/js/login.js"></script>
+</body>
+
+</html>
