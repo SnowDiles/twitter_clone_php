2193dea868f6ec4021e1f5a6a48f519f65688827
SnowDiles <brahimboulahiapro@gmail.com>
2025-03-08 18:01:32 +0100

    [fix] refactore front search page

--- src/Views/search/search.php
+++ src/Views/search/search.php
@@ -6,25 +6,25 @@
 </head>
 
 <body data-theme="my-custom-theme">
-    <div class="flex">
+    <div class="flex ">
         <?php
         require_once '../_partials/_navbar.php';
         ?>
         <div class=" flex-[4] min-h-screen max-h-screen overflow-y-scroll">
             <div class="flex">
-                <div class="flex gap-4 p-4 items-center max-w-xl justify-self-center w-full mr-[6em] border-r border-r-black dark:border-r-white border-b border-b-black dark:border-b-white">
+                <div class="flex gap-10 p-4 items-center max-w-xl justify-self-center w-full border-r border-r-black dark:border-r-white border-b border-b-black dark:border-b-white">
                     <div class="md:invisible">
                         <img src="../../assets/pptest.jpg" alt="profile picture icon" class="h-12 w-12 rounded-full">
                     </div>
                     <div class="relative w-full">
-                        <input type="search" name="search" id="search" placeholder="Rechercher" class="rounded-full p-2 h-[34px] w-[300px] 
+                        <input type="search" name="search" id="search" placeholder="Rechercher" class="rounded-full p-2 h-[34px] w-[230px] md:w-[400px]
                             bg-tertiary dark:variant-filled-tertiary dark:invert text-center md:pl-10 md:text-left ">
-                        <span class="hidden md:block absolute left-3 top-1/2 transform -translate-y-1/2">
+                        <span class=" absolute left-3 top-1/2 transform -translate-y-1/2">
                             <img src="../../assets/icons/search.png" alt="search icon"
-                                class="h-[24px] w-[24px] hidden md:block invert dark:invert-0">
+                                class="h-[24px] w-[24px]  invert dark:invert-0">
                         </span>
                         <div class="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-lg w-64 
-                max-h-64 overflow-y-auto absolute mt-10 z-40" style="display:none" id="hashtag-desktop">
+                max-h-64 overflow-y-auto absolute mt-2 z-40" style="display:none" id="hashtag-desktop">
                             <ul  class="space-y-2">
                             </ul>
                         </div>
