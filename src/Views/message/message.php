d7c98aafd7d18bbe341acdcf7f6b8d7ac77ac4db
SnowDiles <brahimboulahiapro@gmail.com>
2025-03-14 23:34:45 +0100

    [fix] place the icon button at a better spot

--- src/Views/message/message.php
+++ src/Views/message/message.php
@@ -51,7 +51,7 @@
         <div class="w-full">
             <div class="border-t border-black dark:border-white pt-4">
                 <div class="relative flex items-center gap-2">
-                    <input type="text" name="message" id="message-input" placeholder="Envoyer un message"
+                    <input type="text" name="message" id="message-input" placeholder="  Envoyer un message"
                         class="w-full h-10 pl-10 pr-4 py-2 rounded-xl bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2
                          focus:ring-blue-500 dark:focus:ring-blue-400 border border-transparent hover:border-gray-300 dark:hover:border-gray-600 transition-all" />
 
@@ -61,24 +61,17 @@
                             style="width: 100%; height: 100%;"></emoji-picker>
                     </div>
                     <button id="send-message-btn" class="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700">
-                        <svg class="w-6 h-6 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg class="w-6 h-6 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" transform="rotate(90)">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                         </svg>
-
                     </button>
-                    <button id="emoji-toggle" class="invert dark:invert-0 p-2 rounded-full">
+                    
+
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-2">
+                    <button id="emoji-toggle" class="invert dark:invert-0 p-2 pr-4 rounded-full">
                         <img src="../../assets/icons/emoji.png" alt="Ajouter une icone" class="w-6 h-6 min-w-6 min-h-6">
                     </button>
-
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor"
-                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
-                            </path>
-                        </svg>
-
                     </div>
                 </div>
             </div>
